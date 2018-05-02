bfa2d4189f5dafc1c812e4284d1f7bea00f8027b
Makreig <mark@i-lateral.com>
2018-05-02 16:07:41 +0100

    improved code format

--- changelog.md
+++ changelog.md
@@ -1,26 +1,36 @@
 # Changelog
 
 This doucument covers all notable changes and additions.
-## [1.3.2]
+## [1.0]
 
-* Added ability to compile products into single paginated list
-
-## [1.3.1]
-
-* Added option to show/hide categories when calling children
-
-## [1.3]
-
-* Added drag and drop re-ordering to products and categories
-
-## [1.2]
-
-* Added bootstrap classes to template
+* First "stable" release
 
 ## [1.1]
 
 * Minor update to tidy up legacy files
 
-## [1.0]
+## [1.2]
 
-* First "stable" release
+* Added bootstrap classes to template
+
+## [1.3]
+
+* Added drag and drop re-ordering to products and categories
+
+## [1.3.1]
+
+* Added option to show/hide categories when calling children
+
+## [1.3.2]
+
+* Added ability to compile products into single paginated list
+
+
+## [2.0.0]
+
+* converted to ss-4
+
+## [2.0.1]
+
+* initial ss-4 bug fixing
+
--- src/control/CataloguePageController.php
+++ src/control/CataloguePageController.php
@@ -20,14 +20,14 @@ class CataloguePageController extends PageController
     );
     
     /**
-	 * Set up the "restful" URLs
-	 *
-	 * @config
-	 * @var array
-	 */
-	private static $url_handlers = array(
-		'$Action/$ID' => 'handleAction',
-	);
+     * Set up the "restful" URLs
+     *
+     * @config
+     * @var array
+     */
+    private static $url_handlers = array(
+        '$Action/$ID' => 'handleAction',
+    );
 
     public function init()
     {
@@ -37,11 +37,10 @@ class CataloguePageController extends PageController
     public function PaginatedChildren($length = 12)
     {
         return new PaginatedList($this->Children(), $this->request);
-	}
-	
+    }
+    
     public function getCompiledProducts($page_length = 30)
     {
-        $tag = $this->getRequest()->getVar('t');
         $products = false;
         $product_class = Config::inst()->get(
             CataloguePage::class, 
@@ -56,9 +55,7 @@ class CataloguePageController extends PageController
                 'Disabled' => 0
             ];
     
-            if ($tag) {
-                $filter["Tags.URLSegment"] = $tag;
-            }
+            $this->extend('updateCompiledProductsFilter', $filter);
             
             $raw_products = DataList::create($product_class)
                 ->filter(
@@ -68,15 +65,20 @@ class CataloguePageController extends PageController
             $products->setPageLength($page_length);
         }
 
+        $this->extend('updateCompiledProducts', $products);
+
         return $products;
     }
     
     public function product($request)
     {
         // Shift the current url up one and get the URL segment
-		$request->shiftAllParams();
+        $request->shiftAllParams();
         $urlsegment = $request->param('URLSegment');
-        $product_class = CataloguePage::config()->product_class;
+        $product_class = Config::inst()->get(
+            CataloguePage::class, 
+            'product_class'
+        );
         
         // Setup our filter and get a product
         $filter = array(
@@ -96,52 +98,53 @@ class CataloguePageController extends PageController
     }
     
     /**
-	 * Get the appropriate {@link CatalogueProductController} or
+     * Get the appropriate {@link CatalogueProductController} or
      * {@link CatalogueProductController} for handling the relevent
      * object.
-	 *
-	 * @param $object Either Product or Category object
-	 * @param string $action
-	 * @return CatalogueController
-	 */
-	protected static function controller_for($object, $action = null)
+     *
+     * @param $object Either Product or Category object
+     * @param string $action
+     * @return CatalogueController
+     */
+    protected static function controller_for($object, $action = null)
     {
-		if ($object->class == 'CatalogueProduct') {
-			$controller = "CatalogueProductController";
-		} else {
-			$ancestry = ClassInfo::ancestry($object->class);
+        if ($object->class == 'CatalogueProduct') {
+            $controller = "CatalogueProductController";
+        } else {
+            $ancestry = ClassInfo::ancestry($object->class);
             
-			while ($class = array_pop($ancestry)) {
-				if (class_exists($class . "_Controller")) break;
-			}
+            while ($class = array_pop($ancestry)) {
+                if (class_exists($class . "_Controller")) break;
+            }
             
             // Find the controller we need, or revert to a default
-			if($class !== null)
+            if ($class !== null) {
                 $controller = "{$class}_Controller";
-            elseif(ClassInfo::baseDataClass($object->class) == "CatalogueProduct")
+            } else if (ClassInfo::baseDataClass($object->class) == "CatalogueProduct") {
                 $controller = "CatalogueProductController";
-		}
+            }
+        }
 
-		if($action && class_exists($controller . '_' . ucfirst($action))) {
-			$controller = $controller . '_' . ucfirst($action);
-		}
-		
-		return class_exists($controller) ? Injector::inst()->create($controller, $object) : $object;
-	}
+        if($action && class_exists($controller . '_' . ucfirst($action))) {
+            $controller = $controller . '_' . ucfirst($action);
+        }
+        
+        return class_exists($controller) ? Injector::inst()->create($controller, $object) : $object;
+    }
     
     
     /**
-	 * @param SS_HTTPRequest $request
-	 * @param $model
-	 *
-	 * @return HTMLText|SS_HTTPResponse
-	 */
-	protected function handleAction($request, $model)
+     * @param SS_HTTPRequest $request
+     * @param $model
+     *
+     * @return HTMLText|SS_HTTPResponse
+     */
+    protected function handleAction($request, $model)
     {
-		//we return nested controllers, so the parsed URL params need to be discarded for the subsequent controllers
-		// to work
-		$request->shiftAllParams();
+        //we return nested controllers, so the parsed URL params need to be discarded for the subsequent controllers
+        // to work
+        $request->shiftAllParams();
         
-		return parent::handleAction($request, $model);
-	}
+        return parent::handleAction($request, $model);
+    }
 }
